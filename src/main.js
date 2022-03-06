42010d94606d7090f4cd6e36880a5876cfba7066
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-03-06 03:35:11 -0300

    Pegando os valores do input name e checkbox

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import data from "./data/pokemon/pokemon.js";
 
 const allDatas = data.pokemon;
 let formFilters = document.getElementsByClassName("full-checkbox");
-let namePokemon = document.getElementsByClassName("input-name");
 let confirmationButton = document.getElementById("confirm-button");
 let clearButton = document.getElementById("clear-button");
 let resultsType = "";
@@ -15,46 +14,7 @@ let resultName = "";
 
 startSiteOnpokemon();
 
-function startSiteOnpokemon() {
-  let url = Array.from(location.href).join();
-  url = url.replace(/\W/g, "");
-  url = url.includes("filters");
-  if (url === true) {
-    startPageFilters();
-  } else {
-    let containerMain = document.querySelector(".main-home");
-    containerMain.style.height = "";
-    startPageHome();
-  }
-}
-
-function startPageHome() {
-  let rotationPage = false;
-  do {
-    heightWindowHome();
-    rotationPage = screen.orientation.onchange = function (e) {
-      location.reload();
-    };
-  } while (rotationPage == false);
-  let clickPokedex = document.getElementsByClassName("pokedex-close").onclick;
-  if (clickPokedex == true) {
-    window.location.href = "filters.html";
-    startPageFilters();
-  }
-}
-
-function heightWindowHome() {
-  let heightWindow = Number(window.innerHeight);
-  let heightLogo = Number(document.querySelector(".header-home").offsetHeight);
-  let heightText = Number(document.querySelector(".intro-text").offsetHeight);
-  let heightTab = Number(document.querySelector(".details-info").offsetHeight);
-  let sumAll = heightWindow - (heightLogo + heightText + heightTab);
-  let containerMain = document.querySelector(".main-home");
-  containerMain.style.height = sumAll + "px";
-}
-
 function startPageFilters() {
-  heightWindowFilters();
   let clickLogo = document.getElementsByClassName(
     "logo-onpokemon-filter"
   ).onclick;
@@ -85,14 +45,6 @@ function changeFormFilters(e) {
   console.log(e.target.value);
 }
 
-function formName(e) {
-  e.preventDefault();
-  let namePokemon = document.getElementsByClassName("input-name");
-  namePokemon = namePokemon.value.replace(/[^a-z^A-Zà-ú^À-Ú]+/g, "");
-  //teste
-  console.log(namePokemon);
-}
-
 function formCheckbox(e) {
   e.preventDefault();
   let formCheckType = document.forms.formFilters.elements.type;
@@ -107,4 +59,50 @@ function formCheckbox(e) {
   //teste
   console.log(resultsType);
   console.log(resultsWeakness);
+}
+
+function formName(e) {
+  e.preventDefault();
+  let namePokemon = document.querySelector(".input-name").value;
+  resultName = namePokemon.replace(/[^a-z^A-Z^à-ú^À-Ú]/g, "");
+  //teste
+  console.log(resultName);
+}
+
+function heightWindowHome() {
+  let heightWindow = Number(window.innerHeight);
+  let heightLogo = Number(document.querySelector(".header-home").offsetHeight);
+  let heightText = Number(document.querySelector(".intro-text").offsetHeight);
+  let heightTab = Number(document.querySelector(".details-info").offsetHeight);
+  let sumAll = heightWindow - (heightLogo + heightText + heightTab);
+  let containerMain = document.querySelector(".main-home");
+  containerMain.style.height = sumAll + "px";
+}
+
+function startPageHome() {
+  let rotationPage = false;
+  do {
+    heightWindowHome();
+    rotationPage = screen.orientation.onchange = function (e) {
+      location.reload();
+    };
+  } while (rotationPage == false);
+  let clickPokedex = document.getElementsByClassName("pokedex-close").onclick;
+  if (clickPokedex == true) {
+    window.location.href = "filters.html";
+    startPageFilters();
+  }
+}
+
+function startSiteOnpokemon() {
+  let url = Array.from(location.href).join();
+  url = url.replace(/\W/g, "");
+  url = url.includes("filters");
+  if (url === true) {
+    startPageFilters();
+  } else {
+    let containerMain = document.querySelector(".main-home");
+    containerMain.style.height = "";
+    startPageHome();
+  }
 }
