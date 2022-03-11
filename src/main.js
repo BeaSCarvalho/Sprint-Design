1787d57762229b5d57c2000acf2097c3e478c6df
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-03-11 12:44:42 -0300

    Erros encontrados

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import {
-  filterByType,
+  //filterByType,
   filterByWeakness,
   alphabeticOrder,
   orderOfWeakness,
-  percentagePerFilter,
+  //percentagePerFilter,
 } from "./data.js";
 import { startPageHome, startPageFilters } from "./js/start-page.js";
 import { clearName, clearCheckbox } from "./js/clear-form.js";
@@ -21,7 +21,7 @@ const selectOrder = document
 const selectOrderByWeakness = document
   .getElementById("calculation-selector")
   .addEventListener("change", showInOrderOfWeakness);
-const percentage = document.getElementById("quantify-text");
+//const percentage = document.getElementById("quantify-text");
 
 startSiteOnpokemon();
 
@@ -52,42 +52,61 @@ export let formCheckbox = (e) => {
   e.preventDefault();
   for (let i = 0; i < formCheckType.length; i++) {
     if (formCheckType[i].checked) {
-      resultsType += formCheckType[i].value + ",";
+      resultsType += formCheckType[i].value + " ";
     } else if (formCheckWeakness[i].checked) {
-      resultsWeakness += formCheckWeakness[i].value + ",";
+      resultsWeakness += formCheckWeakness[i].value + " ";
     }
   }
   clearCheckbox();
   showResults();
 };
 
+//Arrumar
 function showResults() {
   if (resultsType != "") {
-    resultsType = resultsType.split(",");
+    resultsType = resultsType.split(" ");
     resultsType.pop();
     for (let i = 0; i < resultsType.length; i++) {
-      console.log("chamar funções");
+      activeFilterType(resultsType[i]);
     }
-  } else if (resultsWeakness != "") {
-    resultsWeakness = resultsWeakness.split(",");
+  }
+  if (resultsWeakness != "") {
+    resultsWeakness = resultsWeakness.split(" ");
     resultsWeakness.pop();
     for (let i = 0; i < resultsWeakness.length; i++) {
       console.log("chamar funções");
     }
-  } else {
+  }
+  if (resultName != "") {
     console.log("chamar funções de pesquisar nome");
   }
   addButton();
-  createCards(pokemons);
+  //createCards(pokemons);
 }
 
+//Arrumar funções de filtro
 function activeFilterType(valueType) {
+  console.log("Entrou na função active filter type");
   pokemons = filterByType(pokemons, valueType);
   createCards(pokemons);
+  console.log("Saiu na função active filter type");
 }
+
+function filterByType(data, activeFilter) {
+  console.log("entrou filter by type");
+  return data.filter((item) => {
+    console.log("saiu filter by type");
+    return item.type.includes(activeFilter);
+  });
+}
+
 function activeFilterWeakness(selectedValue) {
+  console.log("Entrou na função filter weak");
   pokemons = filterByWeakness(pokemons, selectedValue);
   createCards(pokemons);
+  //Como já tem criado os cards, não passa nada
+  createColors();
+  console.log("Saiu na função filter weak");
 }
 function orderToShow() {
   pokemons = alphabeticOrder(pokemons, selectOrder.value);
@@ -98,6 +117,7 @@ function showInOrderOfWeakness() {
   createCards(pokemons);
 }
 /*
+//Arrumar
 function showPercentagePerFilter() {
   const showThePercentage = percentagePerFilter(pokemons, pokemons.length);
   percentage.innerHTML = `Esse filtro representa ${showThePercentage}% do total de Pokemons.`;
@@ -132,13 +152,13 @@ function createCards(data) {
             <ul class="poke-items">
               <span class="poke-item-title">Tipo:</span>
               <div class="items">
-                ${colorItems(item.type)}
+                ${item.type}
               </div>
             </ul>
             <ul class="poke-items">
               <span class="poke-item-title">Fraqueza:</span> 
               <div class="items">
-                ${colorItems(item.weaknesses)}
+                ${item.weaknesses}
               </div>
             </ul>
           </main>
@@ -149,69 +169,75 @@ function createCards(data) {
     .join("");
 }
 
-function colorItems(values) {
-  let items = document.querySelector(".items");
-  let list = document.querySelector(".poke-items li");
-  for (let i = 0; i <= values.length; i++) {
+// Arrumar
+function createColors() {
+  let items = document.querySelectorAll(".items");
+  itemsType = Array.from(itemsType);
+
+  for (let i = 0; i <= items.length; i++) {
     let li = document.createElement("li");
-    let value = document.createTextNode(values[i]);
-    li.appendChild(value);
-    items.appendChild(li);
-    switch (values[i]) {
-      case "bug":
-        list.style.backgroundColor = rgb(158, 191, 63);
-        break;
-      case "dark":
-        list.style.backgroundColor = rgb(93, 96, 109);
-        break;
-      case "dragon":
-        list.style.backgroundColor = rgb(21, 116, 196);
-        break;
-      case "electric":
-        list.style.backgroundColor = rgb(238, 212, 79);
-        break;
-      case "fairy":
-        list.style.backgroundColor = rgb(237, 153, 229);
-        break;
-      case "fight":
-        list.style.backgroundColor = rgb(215, 68, 86);
-        break;
-      case "fire":
-        list.style.backgroundColor = rgb(224, 164, 86);
-        break;
-      case "flying":
-        list.style.backgroundColor = rgb(156, 180, 230);
-        break;
-      case "ghost":
-        list.style.backgroundColor = rgb(107, 114, 196);
-        break;
-      case "grass":
-        list.style.backgroundColor = rgb(98, 190, 101);
-        break;
-      case "ground":
-        list.style.backgroundColor = rgb(214, 133, 91);
-        break;
-      case "ice":
-        list.style.backgroundColor = rgb(129, 212, 201);
-        break;
-      case "normal":
-        list.style.backgroundColor = rgb(154, 158, 163);
-        break;
-      case "poison":
-        list.style.backgroundColor = rgb(180, 103, 202);
-        break;
-      case "psychic":
-        list.style.backgroundColor = rgb(247, 124, 124);
-        break;
-      case "rock":
-        list.style.backgroundColor = rgb(205, 192, 144);
-        break;
-      case "stell":
-        list.style.backgroundColor = rgb(89, 150, 163);
-        break;
-      case "water":
-        list.style.backgroundColor = rgb(88, 159, 222);
-        break;
+    let value = document.createTextNode(items[i]);
+
+    for (let j = 0; j <= itemsType.length; j++) {
+      li.appendChild(value);
+      items.appendChild(li);
+
+      switch (itemsType[i]) {
+        case "bug":
+          list.style.backgroundColor = rgb(158, 191, 63);
+          break;
+        case "dark":
+          list.style.backgroundColor = rgb(93, 96, 109);
+          break;
+        case "dragon":
+          list.style.backgroundColor = rgb(21, 116, 196);
+          break;
+        case "electric":
+          list.style.backgroundColor = rgb(238, 212, 79);
+          break;
+        case "fairy":
+          list.style.backgroundColor = rgb(237, 153, 229);
+          break;
+        case "fight":
+          list.style.backgroundColor = rgb(215, 68, 86);
+          break;
+        case "fire":
+          list.style.backgroundColor = rgb(224, 164, 86);
+          break;
+        case "flying":
+          list.style.backgroundColor = rgb(156, 180, 230);
+          break;
+        case "ghost":
+          list.style.backgroundColor = rgb(107, 114, 196);
+          break;
+        case "grass":
+          list.style.backgroundColor = rgb(98, 190, 101);
+          break;
+        case "ground":
+          list.style.backgroundColor = rgb(214, 133, 91);
+          break;
+        case "ice":
+          list.style.backgroundColor = rgb(129, 212, 201);
+          break;
+        case "normal":
+          list.style.backgroundColor = rgb(154, 158, 163);
+          break;
+        case "poison":
+          list.style.backgroundColor = rgb(180, 103, 202);
+          break;
+        case "psychic":
+          list.style.backgroundColor = rgb(247, 124, 124);
+          break;
+        case "rock":
+          list.style.backgroundColor = rgb(205, 192, 144);
+          break;
+        case "stell":
+          list.style.backgroundColor = rgb(89, 150, 163);
+          break;
+        case "water":
+          list.style.backgroundColor = rgb(88, 159, 222);
+          break;
+      }
     }
   }
 }
