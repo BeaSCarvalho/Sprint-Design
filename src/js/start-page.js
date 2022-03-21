596b8e71f6344ee35b635d96a6807deef4171ad6
Adriana Tiemi Watanabe <adriana.t.watanabe@gmail.com>
2022-03-21 17:29:11 -0300

    Testes funcionando

--- src/js/start-page.js
+++ src/js/start-page.js
@@ -1,47 +1,46 @@
-/*import { changeFormName, changeFormCheckbox } from "./change-form.js";
-
 export let startPageHome = () => {
   let rotationPage = false;
   do {
-    heightWindowHome();
+    heightPageHome();
     rotationPage = screen.orientation.onchange = function () {
       location.reload();
     };
   } while (rotationPage == false);
-  let clickPokedex = document.getElementsByClassName("pokedex-close").onclick;
-  if (clickPokedex == true) {
-    window.location.href = "filters.html";
-    startPageFilters();
-  }
+  heightPageHome();
+  initModal();
 };
 
-export let startPageFilters = () => {
-  let clickLogo = document.getElementsByClassName(
-    "logo-onpokemon-filter"
-  ).onclick;
-  if (clickLogo == true) {
-    startPageHome();
-  }
-
-  document
-    .getElementById("name-pokemon")
-    .addEventListener("change", changeFormName);
-  document
-    .getElementById("container-checkbox")
-    .addEventListener("change", changeFormCheckbox);
-  document
-    .getElementById("clear-button")
-    .addEventListener("click", function () {
-      location.reload();
-    });
-};
-
-function heightWindowHome() {
+function heightPageHome() {
   let heightWindow = Number(window.innerHeight);
-  let heightLogo = Number(document.querySelector(".header-home").offsetHeight);
-  let heightText = Number(document.querySelector(".intro-text").offsetHeight);
-  let heightTab = Number(document.querySelector(".details-info").offsetHeight);
-  let sumAll = heightWindow - (heightLogo + heightText + heightTab);
-  let containerMain = document.querySelector(".main-home");
+  let heightLogo = Number(document.getElementById("header-home").offsetHeight);
+  let heightText = Number(document.getElementById("introduction").offsetHeight);
+  let heightContacts = Number(
+    document.getElementById("info-contacts").offsetHeight
+  );
+  let sumAll = heightWindow - (heightLogo + heightText + heightContacts);
+  let containerMain = document.getElementById("main-home");
   containerMain.style.height = sumAll + "px";
-}*/
+}
+
+function initModal() {
+  const openModal = document.querySelector('[data-modal="openModal"]');
+  const closeModal = document.querySelector('[data-modal="closeModal"]');
+  const containerModal = document.querySelector(
+    '[data-modal="containerContacts"]'
+  );
+  if (openModal && closeModal && containerModal) {
+    let toogle = function (event) {
+      event.preventDefault();
+      containerModal.classList.toggle("active");
+    };
+    let outside = function (event) {
+      if (event.target === this) {
+        event.preventDefault();
+        containerModal.classList.toggle("active");
+      }
+    };
+    openModal.addEventListener("click", toogle);
+    closeModal.addEventListener("click", toogle);
+    containerModal.addEventListener("click", outside);
+  }
+}
