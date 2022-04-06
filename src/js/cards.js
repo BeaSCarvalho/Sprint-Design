36daa85cb2de85d07f387b84b9a86a4d578ca9e5
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-04-06 04:28:49 -0300

    Cards aparecendo na tela

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,38 +1,36 @@
 const resultCards = document.getElementById("result-cards");
 let itemsType = [];
-let itemsWeakness = [];
 
 export let createCards = (data) => {
   itemsType = [];
-  itemsWeakness = [];
   resultCards.innerHTML = data
     .map((item) => {
       itemsType.push(item.type);
-      itemsWeakness.push(item.weaknesses);
       return `
       <div class="card">
         <p class="poke-number">${item.num}</p>
         <div class="card-box">
-          <figure class="box-poke-img">
-            <img class="poke-img" src="${item.img}" alt=${item.name}>
-          </figure>
-          <h4 class="poke-title"> 
-            ${item.name[0].toUpperCase() + item.name.substr(1)}
-          </h4>
-          <ul class="poke-items type">
-            <span class="list-title list-type">Tipo:</span>
-          </ul>
-          <ul class="poke-items weakness">
-            <span class="list-title list-weakness">Fraqueza:</span> 
-          </ul>
+          <img class="poke-img" src="${item.img}" alt=${item.name}>
         </div>
+        <h4 class="poke-title">
+          ${item.name[0].toUpperCase() + item.name.substr(1)}
+        </h4>
+        <ul class="poke-items list-type">
+        </ul>
+        <button type="button" class="small-button">
+          open
+          <div class="container-icon" data-modal="open-modal">
+            <img src="./img/icon-open.png" alt="More information" class="small-icon">
+          </div>
+        </button>
       </div>
       `;
     })
     .join("");
   createLiType();
 };
-export let createLiType = () => {
+let createLiType = () => {
+  let number = document.querySelectorAll(".poke-number");
   let listType = document.querySelectorAll(".list-type");
   for (let j = 0; j < itemsType.length; j++) {
     let itemsT = itemsType[j];
@@ -43,130 +41,61 @@ export let createLiType = () => {
       li.classList.add("li-item");
 
       if (li.textContent === "bug") {
-        li.textContent = "inseto";
         li.classList.add("bug");
+        number[j].classList.add("bug");
       } else if (li.textContent === "dark") {
-        li.textContent = "sombra";
         li.classList.add("dark");
+        number[j].classList.add("dark");
       } else if (li.textContent === "dragon") {
-        li.textContent = "dragão";
         li.classList.add("dragon");
+        number[j].classList.add("dragon");
       } else if (li.textContent === "electric") {
-        li.textContent = "elétrico";
         li.classList.add("electric");
+        number[j].classList.add("electric");
       } else if (li.textContent === "fairy") {
-        li.textContent = "fada";
         li.classList.add("fairy");
+        number[j].classList.add("fairy");
       } else if (li.textContent === "fighting") {
-        li.textContent = "lutador";
         li.classList.add("fighting");
+        number[j].classList.add("fighting");
       } else if (li.textContent === "fire") {
-        li.textContent = "fogo";
         li.classList.add("fire");
+        number[j].classList.add("fire");
       } else if (li.textContent === "flying") {
-        li.textContent = "voador";
         li.classList.add("flying");
+        number[j].classList.add("flying");
       } else if (li.textContent === "ghost") {
-        li.textContent = "fantasma";
         li.classList.add("ghost");
+        number[j].classList.add("ghost");
       } else if (li.textContent === "grass") {
-        li.textContent = "planta";
         li.classList.add("grass");
+        number[j].classList.add("grass");
       } else if (li.textContent === "ground") {
-        li.textContent = "terra";
         li.classList.add("ground");
+        number[j].classList.add("ground");
       } else if (li.textContent === "ice") {
-        li.textContent = "gelo";
         li.classList.add("ice");
+        number[j].classList.add("ice");
       } else if (li.textContent === "normal") {
-        li.textContent = "normal";
         li.classList.add("normal");
+        number[j].classList.add("normal");
       } else if (li.textContent === "poison") {
-        li.textContent = "venenoso";
         li.classList.add("poison");
+        number[j].classList.add("poison");
       } else if (li.textContent === "psychic") {
-        li.textContent = "psíquico";
         li.classList.add("psychic");
+        number[j].classList.add("psychic");
       } else if (li.textContent === "rock") {
-        li.textContent = "pedra";
         li.classList.add("rock");
+        number[j].classList.add("rock");
       } else if (li.textContent === "steel") {
-        li.textContent = "aço";
         li.classList.add("steel");
+        number[j].classList.add("steel");
       } else if (li.textContent === "water") {
-        li.textContent = "água";
         li.classList.add("water");
-      }
-    }
-  }
-  createLiWeakness();
-};
-
-export let createLiWeakness = () => {
-  let listWeakness = document.querySelectorAll(".list-weakness");
-  for (let j = 0; j < itemsWeakness.length; j++) {
-    let itemsW = itemsWeakness[j];
-    for (let i = 0; i < itemsW.length; i++) {
-      const li = document.createElement("li");
-      listWeakness[j].after(li);
-      li.textContent = `${itemsW[i]}`;
-      li.classList.add("li-item");
-
-      if (li.textContent === "bug") {
-        li.textContent = "inseto";
-        li.classList.add("bug");
-      } else if (li.textContent === "dark") {
-        li.textContent = "sombra";
-        li.classList.add("dark");
-      } else if (li.textContent === "dragon") {
-        li.textContent = "dragão";
-        li.classList.add("dragon");
-      } else if (li.textContent === "electric") {
-        li.textContent = "elétrico";
-        li.classList.add("electric");
-      } else if (li.textContent === "fairy") {
-        li.textContent = "fada";
-        li.classList.add("fairy");
-      } else if (li.textContent === "fighting") {
-        li.textContent = "lutador";
-        li.classList.add("fighting");
-      } else if (li.textContent === "fire") {
-        li.textContent = "fogo";
-        li.classList.add("fire");
-      } else if (li.textContent === "flying") {
-        li.textContent = "voador";
-        li.classList.add("flying");
-      } else if (li.textContent === "ghost") {
-        li.textContent = "fantasma";
-        li.classList.add("ghost");
-      } else if (li.textContent === "grass") {
-        li.textContent = "planta";
-        li.classList.add("grass");
-      } else if (li.textContent === "ground") {
-        li.textContent = "terra";
-        li.classList.add("ground");
-      } else if (li.textContent === "ice") {
-        li.textContent = "gelo";
-        li.classList.add("ice");
-      } else if (li.textContent === "normal") {
-        li.textContent = "normal";
-        li.classList.add("normal");
-      } else if (li.textContent === "poison") {
-        li.textContent = "venenoso";
-        li.classList.add("poison");
-      } else if (li.textContent === "psychic") {
-        li.textContent = "psíquico";
-        li.classList.add("psychic");
-      } else if (li.textContent === "rock") {
-        li.textContent = "pedra";
-        li.classList.add("rock");
-      } else if (li.textContent === "steel") {
-        li.textContent = "aço";
-        li.classList.add("steel");
-      } else if (li.textContent === "water") {
-        li.textContent = "água";
-        li.classList.add("water");
+        number[j].classList.add("water");
       }
     }
   }
 };
+
