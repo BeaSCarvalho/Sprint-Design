dbd13e8c9427b4cf9d9bc6fe4f54164f93380c8a
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-04-07 01:03:51 -0300

    Alterações na main do filter

--- src/pages/filters/main.js
+++ src/pages/filters/main.js
@@ -4,9 +4,32 @@ import {
   orderOfWeakness,
   percentagePerFilter,
   filterBy,
+  filterRarity,
 } from "./js/data.js";
 import { createCards } from "./js/cards.js";
 import data from "../../data/pokemon/pokemon.js";
+
+const buttonMobile = document.getElementById("button-mobile");
+
+buttonMobile.addEventListener("click", toggleMenu);
+buttonMobile.addEventListener("touchstart", toggleMenu);
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
+
+  const navigation = document.getElementById("nav");
+  navigation.classList.toggle("active");
+  const navActive = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", navActive);
+
+  if (navActive) {
+    event.currentTarget.setAttribute("aria-laberl", "Close Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-laberl", "Open Menu");
+  }
+}
 
 const nameTyped = document.getElementById("name-pokemon");
 const selectTypeOrWeakness = document.getElementById("filter-type-weakness");
@@ -16,6 +39,7 @@ const selectOrderByWeakness = document.getElementById("calculation-selector");
 const cleanButton = document.getElementById("clean-button");
 const percentage = document.getElementById("quantify-text");
 const resultCards = document.getElementById("result-cards");
+const selectRarity = document.getElementById("rarity-selector");
 
 startPageFilters();
 
@@ -39,6 +63,10 @@ function startPageFilters() {
   selectOrderByWeakness.addEventListener("change", function () {
     selectOrderByWeakness.classList.replace("color-select", "new-color-select");
     showInOrderOfWeakness();
+  });
+  selectRarity.addEventListener("change", function () {
+    selectRarity.classList.replace("color-select", "new-color-select");
+    filterPerRarity();
   });
   typeWriter(percentage);
   cleanButton.addEventListener("click", cleanForm);
@@ -71,12 +99,14 @@ function orderToShow() {
   const selectedOrder = selectOrder.value;
   createCards(alphabeticOrder(data.pokemon, selectedOrder));
   activeFilters(alphabeticOrder(data.pokemon, selectedOrder));
+  filterPerRarity(alphabeticOrder(data.pokemon, selectedOrder));
 }
 
 function showInOrderOfWeakness() {
   const selectedOrderByWeakness = selectOrderByWeakness.value;
   createCards(orderOfWeakness(data.pokemon, selectedOrderByWeakness));
   activeFilters(orderOfWeakness(data.pokemon, selectedOrderByWeakness));
+  filterPerRarity(orderOfWeakness(data.pokemon, selectedOrderByWeakness));
 }
 
 function percentagePokemon(resultOfFilters, pokemon) {
@@ -107,4 +137,12 @@ function cleanForm() {
   selectOrder.selectedIndex = 0;
   const selectedOrder = selectOrder.value;
   createCards(alphabeticOrder(data.pokemon, selectedOrder));
+}
+
+function filterPerRarity() {
+  let selectedValueRarity = selectRarity.value;
+  let resultOfFilters = filterRarity(data.pokemon, selectedValueRarity);
+  createCards(resultOfFilters);
+  resultOfFilters = resultOfFilters.length;
+  percentagePokemon(resultOfFilters, data.pokemon);
 }
