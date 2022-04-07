f86e6f307f0175d64912d161ebbdbec55fe0b2c9
Helena <nannayusuf@gmail.com>
2022-04-07 01:49:35 -0300

    caminho de imagens corrigidas, página curiosities.js criada

---
+++ src/data/pokemon/curiosities.js
@@ -0,0 +1,51 @@
+export default {
+
+    "post": [{
+        "título": "1. The first Pokémon ever designed",
+        "text": " It may be entry #112 in the Pokédex, but according to Ken Sugimori – the primary designer for the Pokémon games – Rhydon was the first Pokémon ever created. This is also the reason why sprites of Rhydon were so widespread in the original games. In that same interview, Sugimori mentions Lapras and Clefairy as some of the other earliest Pokémon designs to join Rhydon. You cand read more about it here!",
+        "img": "https://img.redbull.com/images/c_fill,g_auto,w_1040,h_693/q_auto,f_auto/redbullcom/2017/09/13/f5abb4bc-79a4-4d8a-9f99-d094ed7eb4ab/rhydon-in-early-concept-designs.jpg",
+        "caption": "The first sketch of Rhydon, the first pokémon ever created."
+    }
+    {
+        "título": "2. Pokémon doesn't have a plural",
+        "text": "What does Pokemon mean? The name Pokémon comes from the word combination of the original Japanese title: Pocket Monsters - which also explains why the term Pokémon has no plural.",
+        "img": "https://i.pinimg.com/originals/54/bc/bc/54bcbcd0ea744587f30df0fea3bf354b.gif",
+    },
+    {
+        "título": "3. Slowbro Is The Only Pokemon That Can Devolve"
+        "img": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/Slowpoke-and-Slowbro-Pokemon.jpg?q=50&fit=crop&w=740&dpr=1.5"
+    "text": "its previous evolution Slowpoke.The creature initially evolves when a Shellder latches on to Slowpoke\'s tail, and it seems like the process is reversed if the shell Pokemon ever lets go."
+    }{
+        "título""4. The gender of a Pikachu can be determined by its tail.",
+        "img": "image/Pikachu-Pop_Star.png",
+        "caption": "Pikachu Pop Star as seen on Pokemon Go Fest 2021",
+        "text": "If the end of its tail is long and flat, it means that it is a male. The tail of the female species is shaped like a heart at its tip. However, when they evolve into Raichu, their tails look more similar to each other."
+    }
+{
+        "título": "5. Lots of Pokémon names are puns",
+        "img": "https://i.pinimg.com/originals/17/81/a1/1781a138e4417b7fba241dfc90a54225.gif",
+        "caption": "Jigglypuff: As in, “An Irish jigglypuff” and “The jigglypuff is up”",
+
+        "text": "For example, the powers of Mimikyu are literally to 'mimic you'. And Sudowoodo? 'Psudo wood', or fake wood. Squirtle is a reference to the fact that he's a turtle who can squirt water, and Charmander is so -called because it's a fire Pokémon which can 'char' you. Which other clever names can you spot? If not, https://punpedia.org/tag/jigglypuff/ here's a list of all the Pokémon puns!"
+    }
+
+{
+        "título": "6. The 'Gotta Catch 'Em All' ethos has a back story",
+        "img": "https://i.pinimg.com/564x/77/fb/5b/77fb5b6fa2b4fb6f81d71984bd98dfff.jpg",
+        "text": "Satoshi\'s idea about collecting animals in Pokémon came from his love of being in the garden as a child. As a boy, he would spend hours finding and collecting bugs and insects.In fact he spent so much time doing it, his friends called him \'Dr. Bug\', and he originally wanted to become a bug scientist - or entomologist, to give it its proper name.We\'re glad he decided to work in video games instead!"
+    }
+{
+        "título": "7. The colours of the Pokeballs are actually based on Cambell\'s soup cans",
+        "img": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cans-of-campbells-soup-sit-on-a-shelf-in-a-grocery-store-on-news-photo-1046599136-1542810801.jpg",
+        "text": "Yes, you read right, the famous pokeball is actually meant to mimic the look of a soup can! Specifically, the tomato soup cans that appear in Andy Warhol\'s famous picture. We\'re not sure why the design was based on a soup can, but once you see it, you can't unsee.",
+    }
+{
+    "título": "8. Ash's Pokémon was originally meant to be a Clefairy, not Pikachu",
+    "text": "It\'s hard to imagine anyone else as Ash\'s best pal, but the original plan was to make it a Clefairy! It\'s easy to see why, like Pikachu Clefairies are cute, small and portable, but 'Detective Clefairy' just doesn\'t have the same ring to it."
+    }
+{
+        "título": "9. Over 88 billion Pokémon have been caught on Pokémon Go",
+        "img": "https://www.beano.com/wp-content/uploads/2021/03/8-19.png?w=977&strip=all&quality=85",
+        "text": "That's a lot of Pokémon! Between millions of different people of course - no one person has caught that many! In fact, it's basically impossible to 'Catch 'Em All' since some Pokémon don't actually appear in the game, especially some of the rarer or older ones.It's fun trying to find them though! Ivysaur is the only second - stage grass - poison evolution that does not require an item to reach its final formOther Grass - Poison types in the Kanto region require a special stone to achieve its final or alternate forms. An example of this is getting Gloom to evolve into Bellossom."
+    }
+}
